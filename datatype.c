d5525287359bc724e857023cc4e5545a358bbfec
KeyurPatel <157466601+keyurpatel01@users.noreply.github.com>
2026-02-25 13:42:24 +0000

    21feb

---
+++ datatype.c
@@ -0,0 +1,21 @@
+/*
+Datatype : type of data
+int = whole numbers
+float = decimal numbers
+double = large decimal
+char = single character
+void = no value
+*/
+
+#include<stdio.h>
+int main(){
+    int age = 20;
+    float height = 4.3;
+    char grade = 'A';
+    double salary = 83333.33333;
+    printf("\n age is %d",age);//int
+    printf("\n height is %f",height);//float
+    printf("\n grade is %c",grade);//character
+    printf("\n salary is %lf",salary);//double
+    return 0;
+}
---
+++ table.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    printf("2x1=%d\n",2*1);
+    printf("2x2=%d\n",2*2);
+    printf("2x3=%d\n",2*3);
+    printf("2x4=%d\n",2*4);
+    printf("2x5=%d\n",2*5);
+    printf("2x6=%d\n",2*6);
+    printf("2x7=%d\n",2*7);
+    printf("2x8=%d\n",2*8);
+    printf("2x9=%d\n",2*9);
+    printf("2x10=%d\n",2*10);
+
+    return 0;
+
+}
