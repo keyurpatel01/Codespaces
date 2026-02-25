5cd13b015cbf6167728dcac5cb71e0d27e1b8562
KeyurPatel <157466601+keyurpatel01@users.noreply.github.com>
2026-02-25 13:38:17 +0000

    19feb

---
+++ first.c
@@ -0,0 +1,7 @@
+#include<stdio.h> //stdio : standard input output
+int main(){
+    printf("Hello from Tops..\n");
+    printf("I am Keyur\n");
+    printf("This is not new line!/n");
+    return 0;
+}
