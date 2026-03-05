3015b3be14965bfbee7c44cbaf6db73911b24a06
KeyurPatel <157466601+keyurpatel01@users.noreply.github.com>
2026-03-05 04:20:07 +0000

    24&26feb

---
+++ operators.c
@@ -0,0 +1,69 @@
+/*
+Arithmetic Operator : used for calculation
++,-,*,/,%
+
+12345 % 10 to get last number.
+*/
+#include <stdio.h>
+int main()
+{
+    int num_1 = 10, num_2 = 20;
+    printf("\n Num is %d", num_1 + num_2);
+    printf("\n Num is %d", num_1 - num_2);
+    printf("\n Num is %d", num_1 * num_2);
+    printf("\n Num is %d", num_1 / num_2);
+    printf("\n Num is %d", num_1 % num_2);
+
+    /*
+    Assignment Operator: =,+=,-=
+
+    */
+    int num1 = 5;
+    num1 += 3;
+    printf("%d", num1);
+
+    int num2 = 10;
+    num2 -= 2;
+    printf("%d", num2);
+
+    /*
+    Logical Operator : &&,||,!
+    */
+
+    int a = 10;
+    printf("\n%d", a > 2 && a < 20);
+    printf("\n%d", a > 2 || a < 10);
+    printf("\n%d", !(a > 0));
+
+    /*
+    Relational Operator : <,>,<=,>=,==,!=
+    */
+
+    int a = 10, b = 20;
+    printf("\n%d\n", a > b);
+    printf("\n%d\n", a < b);
+    printf("\n%d\n", a >= b);
+    printf("\n%d\n", a <= b);
+    printf("\n%d\n", a == b);
+    printf("\n %d \n", a != b);
+
+    /*
+    Ternery Operator :
+    */
+    int marks = 49;
+    char result = (marks >= 33) ? 'p' : 'f';
+    printf("\nresult is %c\n", result);
+
+    int no1 = 85, no2 = 410;
+    // if (no1 > no2)
+    // {
+    //     printf("\nmax no is %d\n", no1);
+    // }else{
+    //     printf("\nmax no is %d\n", no2);
+
+    // }
+    int max = (no1 > no2) ? no1 : no2;
+    printf("max no is %d", max);
+
+    return 0;
+}
